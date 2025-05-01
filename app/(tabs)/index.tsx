95b7dce1fe12b2bc83eb3f0074a3e2e2fbf5a046
lucaslee129 <adfr486233@gmail.com>
2025-05-01 16:11:11 +0800

    feat: complete the scroll view

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { Alert, Image, StyleSheet, Platform, View, Text, Button, FlatList } from 'react-native';
+import { Alert, Image, StyleSheet, Platform, View, Text, Button, FlatList, ScrollView } from 'react-native';
 import { Icon } from '@rneui/themed';
 import { StatusBar } from 'expo-status-bar';
 
@@ -16,26 +16,49 @@ export default function HomeScreen() {
     <View style = {listViewStyle.container}>
       {/* <Button onPress = {onPressButton} title="Press Me" color="green" />
       <StatusBar style='auto' /> */}
-      <FlatList
-        data={[
-          {key: 'Devin'},
-          {key: 'Dan'},
-          {key: 'Dominic'},
-          {key: 'Jackson'},
-          {key: 'James'},
-          {key: 'Joel'},
-          {key: 'John'},
-          {key: 'Jillian'},
-          {key: 'Jimmy'},
-          {key: 'Julie'},
-        ]}
-        renderItem={({item}) => (
-          <View style={{flexDirection: 'row', alignItems: 'center'}}>
-            <Text style={listViewStyle.item}>{item.key}</Text>
-            <Icon name='eye-outline' type='ionicon' color='green' size={20} />
-          </View>
-        )}
-      />
+      <ScrollView>
+        <FlatList
+          data={[
+            {key: 'Devin'},
+            {key: 'Dan'},
+            {key: 'Dominic'},
+            {key: 'Jackson1'},
+            {key: 'James1'},
+            {key: 'Joel1'},
+            {key: 'John1'},
+            {key: 'Jillian1'},
+            {key: 'Jimmy1'},
+            {key: 'Julie1'},
+            {key: 'Jackson2'},
+            {key: 'James2'},
+            {key: 'Joel2'},
+            {key: 'John2'},
+            {key: 'Jillian2'},
+            {key: 'Jimmy2'},
+            {key: 'Julie2'},
+            {key: 'Jackson3'},
+            {key: 'James3'},
+            {key: 'Joel3'},
+            {key: 'John3'},
+            {key: 'Jillian3'},
+            {key: 'Jimmy3'},
+            {key: 'Julie3'},
+            {key: 'Jackson4'},
+            {key: 'James4'},
+            {key: 'Joel4'},
+            {key: 'John4'},
+            {key: 'Jillian4'},
+            {key: 'Jimmy4'},
+            {key: 'Julie4'},
+          ]}
+          renderItem={({item}) => (
+            <View style={{flexDirection: 'row', alignItems: 'center'}}>
+              <Text style={listViewStyle.item}>{item.key}</Text>
+              <Icon name='eye-outline' type='ionicon' color='green' size={20} />
+            </View>
+          )}
+        />
+      </ScrollView>
     </View>
   );
 }
